db1e0c97335a005a0ac63ea3eeee8c4fa79c8193
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-04-02 12:59:45 +0200

    Modal fix, indenting
    Now the modal pops up only when one of our countries is selected

--- javascript.js
+++ javascript.js
@@ -13,50 +13,53 @@ var map = new Datamap({
         CZE: { fillKey: "authorHasTraveledTo" },
         DNK: { fillKey: "authorHasTraveledTo" },
         POL: { fillKey: "authorHasTraveledTo" },
-      },
-    setProjection: function(element) {
-    var projection = d3.geo.mercator()
-        .center([10.166667, 52.4444])
-        .scale(500)
-        .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    },
+    setProjection: function (element) {
+        var projection = d3.geo.mercator()
+            .center([10.166667, 52.4444])
+            .scale(500)
+            .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
         var path = d3.geo.path().projection(projection);
-        return {path: path, projection: projection};
+        return { path: path, projection: projection };
     },
     geographyConfig: {
-    dataUrl: null, //if not null, datamaps will fetch the map JSON (currently only supports topojson)
-    hideAntarctica: true,
-    borderWidth: 1,
-    borderOpacity: 1,
-    borderColor: '#eee',
-    popupTemplate: function(geography, data) { //this function should just return a string
-        return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong></div>';
+        dataUrl: null, //if not null, datamaps will fetch the map JSON (currently only supports topojson)
+        hideAntarctica: true,
+        borderWidth: 1,
+        borderOpacity: 1,
+        borderColor: '#eee',
+        popupTemplate: function (geography, data) { //this function should just return a string
+            return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong></div>';
+        },
+        popupOnHover: true, //disable the popup while hovering
+        highlightOnHover: true,
+        highlightFillColor: '#FC8D59',
+        highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+        highlightBorderWidth: 2,
+        highlightBorderOpacity: 1
     },
-    popupOnHover: true, //disable the popup while hovering
-    highlightOnHover: true,
-    highlightFillColor: '#FC8D59',
-    highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
-    highlightBorderWidth: 2,
-    highlightBorderOpacity: 1
-    },
-    done: function(datamap) {
-        datamap.svg.selectAll('.datamaps-subunit').on('click', function() {
-                console.log('clicked')
+    done: function (datamap) {
+        datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
+            console.log(geography.id)
 
+            let our_countries = ['CZE', 'DNK', 'GBR', 'HUN', 'POL'];
+            if (our_countries.includes(geography.id)) {
                 // Grab the modal and display it
                 var modal = document.getElementById("myModal");
                 modal.style.display = "block";
 
                 // When the user clicks on <span> (x), close the modal
                 var span = document.getElementsByClassName("close")[0];
-                span.onclick = function() {
+                span.onclick = function () {
                     modal.style.display = "none";
                 }
 
                 // When the user clicks anywhere outside of the modal, close it
-                window.onclick = function(event) {
-                if (event.target == modal) {
-                    modal.style.display = "none";
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
                 }
             }
         });
@@ -67,6 +70,6 @@ var map = new Datamap({
 console.log(pol)
 
 document.addEventListener('click', function (event) {
-	if (!event.target.closest('.POL')) return;
-	console.log('Clicked');
+    if (!event.target.closest('.POL')) return;
+    console.log('Clicked');
 }, false); */
